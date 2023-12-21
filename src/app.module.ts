eff4ddb95c43b1dc4d0f0bdc6818f4cb82c681ee
Ifazo <ziaulkarimifaz@outlook.com>
2023-12-21 17:16:37 +0600

    prisma and neon init

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ProductsModule } from './products/products.module';
+import { CategoriesModule } from './categories/categories.module';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [],
+  imports: [ProductsModule, CategoriesModule, UsersModule, AuthModule, DatabaseModule],
   controllers: [AppController],
   providers: [AppService],
 })
